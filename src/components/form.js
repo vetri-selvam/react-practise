4a343ded58d77ff60633ed1e3d1b3424e6518b2f
Vetri Selvam <vetriwork1@gmail.com>
2024-10-11 10:26:46 +0530

    using object in useState and accessing via form

--- src/components/form.js
+++ src/components/form.js
@@ -1,31 +1,56 @@
-import { useState } from "react"
+import { useState } from "react";
 
+function Form() {
+  const [details, setDetails] = useState({});
 
+  //console.log("Current Name: ",name);
 
-function Form(){
+  function doWhenSubmitted(e) {
+    e.preventDefault();
+    console.log("Form Submitted");
+    console.log("Current Name, age and Country: ", details);
+  }
 
-    const [name, setName] = useState("");
-
-    //console.log("Current Name: ",name);
-
-    function doWhenSubmitted(e) {
-        e.preventDefault();
-        console.log("Form Submitted");
-        console.log("Current Name: ",name);
-    }
-
-    return (
-        <>
-            <form onSubmit={doWhenSubmitted}>
-                Enter your name: 
-                <input type="text" onChange={ (e)=>{setName(e.target.value)} }/>
-                <input type="submit" value="Submit"/>
-
-            </form>
-
-        </>
-        
-    )
+  return (
+    <>
+      <form onSubmit={doWhenSubmitted}>
+        Enter your name:
+        <input
+          type="text"
+          onChange={(e) => {
+            setDetails((previousState) => {
+              // Using `previousState` for keeping the rest of the state unchanged while ppdating the 'name' field.
+              return { ...previousState, name: e.target.value };
+            });
+          }}
+        />
+        <br />
+        Enter your age:
+        <input
+          type="text"
+          onChange={(e) => {
+            setDetails((previousState) => {
+              // Update the 'age' field while keeping the rest of the state unchanged
+              return { ...previousState, age: e.target.value };
+            });
+          }}
+        />
+        <br />
+        Enter your country:
+        <input
+          type="text"
+          onChange={(e) => {
+            setDetails((previousState) => {
+              // Update the 'country' field while keeping the rest of the state unchanged
+              return { ...previousState, country: e.target.value };
+            });
+          }}
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  );
 }
 
 export default Form;
